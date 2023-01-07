5a3b58ea3138fe53c848c7f85077004a145dc90c
LiuJun <liujun001217@gmail.com>
2023-01-08 02:29:04 +0800

    [feat] login init

--- smartOps/views.py
+++ smartOps/views.py
@@ -12,6 +12,8 @@ from django.contrib.auth import authenticate
 ADMIN_NAME='trade'
 ADMIN_PASSWD=''
 
+Copyright="Copyright CFFEX SmartOps V1.0"
+
 def obj_to_dict(obj_list):
     result = []
     for obj in obj_list:
@@ -40,21 +42,31 @@ def chk_login(name, passwd) -> str:
     else:
         return None
 
+
+def so_render(request, html: str = ""):
+    if html=="":
+        log_info("ERROR")
+    return render(request, html, {"Copyright": Copyright})
+
+
+
+
+
 def login(request):
     log_info(request)
     if request.method == 'GET':
-        return render(request, 'login.html')
+        return so_render(request,"user/login.html")
 
     chk_rst = chk_login(request.POST['name'], request.POST['password'])
     if chk_rst != None:
-        return render(request, 'login.html', {'errmsg': chk_rst})
+        return render(request, 'user/login.html', {'errmsg': chk_rst})
 
     return index(request) 
 
 
 def index(request):
     log_info(str(request))
-    return render(request, 'index.html')
+    return so_render(request, 'index/index.html')
 
 def welcome(request):
     log_info(str(request))
